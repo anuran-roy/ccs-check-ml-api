e145257ae168873b54eea0b57e7b4dd59bc43dd4
anuran <anuranroy02@gmail.com>
2022-03-20 17:56:47 +0530

    Added scoring mechanism

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from routes import check
 app = FastAPI()
 app.include_router(check.router)
 
+
 @app.get("/")
 async def hello():
     return {"message": "Hello"}
-
-
-
--- scripts/utils/scorer.py
+++ scripts/utils/scorer.py
@@ -1,24 +1,42 @@
 from models.descriptive import DescriptiveModel
+import datetime
+import spacy
 
 
 class Scorer:
     def __init__(self):
-        pass
+        try:
+            self.nlp = spacy.load("en_core_web_sm")
+        except Exception:
+            spacy.cli.download("en_core_web_sm")
+            self.nlp = spacy.load("en_core_web_sm")
 
     def check_similarity(self, model_answer, given_answer):
-        return 1
+        model_answer_token = self.nlp(model_answer)
+        given_answer_token = self.nlp(given_answer)
+
+        return given_answer_token.similarity(model_answer_token)
 
     def check_keywords(self, given_answer, keywords):
-        return 1
+        if keywords is []:
+            return 1
+
+        received_keywords = [x.lemma_ for x in self.nlp(given_answer)]
+        common_keywords = set(keywords).intersection(set(received_keywords))
+        return len(common_keywords) / len(set(keywords))  # Score
 
     def sentiment_similarity(self, model_answer, given_answer):
         return 1
 
     def score(self, answer: DescriptiveModel):
         weighted_score: float = (
-            0.2*self.check_similarity(answer.model_answer, answer.given_answer)
-            + 0.4*self.check_keywords(answer.given_answer, answer.keywords)
-            + 0.4*self.sentiment_similarity(answer.model_answer, answer.given_answer)
+            # 0.2 * self.check_similarity(
+            #     answer.model_answer, answer.given_answer
+            # ) +
+            0.6 * self.check_keywords(answer.given_answer, answer.keywords)
+            + 0.4 * self.check_similarity(
+                answer.model_answer, answer.given_answer
+            ) 
         )
-        
-        return weighted_score*answer.weightage
+
+        return weighted_score * answer.weightage
